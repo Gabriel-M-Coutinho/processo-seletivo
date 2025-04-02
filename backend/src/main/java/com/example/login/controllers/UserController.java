c291ad3fdc3573cadf01499a3f0734813bc87917
Gabriel Coutinho <122622768+Gabriel-M-Coutinho@users.noreply.github.com>
2025-04-02 19:12:41 -0300

    config dockerContainers

---
+++ backend/src/main/java/com/example/login/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.example.login.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World";
+    }
+}
