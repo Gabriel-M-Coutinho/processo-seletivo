2f507b2067a61019e033cb533005e3404ecb82cc
Gabriel Coutinho <122622768+Gabriel-M-Coutinho@users.noreply.github.com>
2025-04-02 21:33:45 -0300

    created Model user and repositorie

---
+++ backend/src/main/java/com/example/login/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.login.repositories;
+
+import com.example.login.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User,String> {
+
+}
